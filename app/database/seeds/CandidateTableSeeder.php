bc499cddc7e55c363697c588ddc3e8f2498eace2
Juan Ruiz <juanruizm001@gmail.com>
2015-05-22 18:07:23 -0300

    Actualizacion de archivos seeders para carga a BD de forma aleatoria con ''

--- app/database/seeds/CandidateTableSeeder.php
+++ app/database/seeds/CandidateTableSeeder.php
@@ -11,21 +11,23 @@ class CandidateTableSeeder extends Seeder {
 
 		foreach(range(1, 80) as $index)
 		{
+            $fullName = $faker->name;
+
             $user = User::create([
-                'full_name' => '',
-                'email'     => '',
-                'password'  => '',
+                'full_name' => $fullName,
+                'email'     => $faker->email,
+                'password'  => \Hash::make(123456),
                 'type'      => 'candidate'
             ]);
 
 			Candidate::create([
                 'id'            => $user->id,
-                'website_url'   => '',
-                'desciption'    => '',
-                'job_type'      => '',
-                'category_id'   => '',
+                'website_url'   => $faker->url,
+                'description'   => $faker->text(200),
+                'job_type'      => $faker->randomElement(['full', 'partial', 'freelance']),
+                'category_id'   => $faker->randomElement([1, 2, 3]),
                 'available'     => true,
-                'slug'          => \Str::slug()
+                'slug'          => \Str::slug($fullName)
 
 
             ]);
